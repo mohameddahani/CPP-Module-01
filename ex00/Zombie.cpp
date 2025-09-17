7a1271e0ce2954444851eb80d4ecdbfcfaf2cf3c
Mohamed Dahani <mohamedxdahani@gmail.com>
2025-09-17 10:43:08 +0100

    Definitions of Constructor, Destructor, Member functions, and Setters, Getters

--- ex00/Zombie.cpp
+++ ex00/Zombie.cpp
@@ -6,7 +6,29 @@
 /*   By: mdahani <mdahani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/17 09:45:21 by mdahani           #+#    #+#             */
-/*   Updated: 2025/09/17 09:45:22 by mdahani          ###   ########.fr       */
+/*   Updated: 2025/09/17 10:40:57 by mdahani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Zombie.hpp"
+
+// ! Definitions of Constructor, Destructor, Member functions, and Setters, Getters
+void Zombie::setName(std::string value){
+    this->name = value;
+}
+
+std::string Zombie::getName(){
+    return this->name;
+}
+
+Zombie::Zombie(std::string nameOfZombie){
+    this->setName(nameOfZombie);
+}
+
+Zombie::~Zombie(){
+    std::cout << this->getName() << " is destroyed" << std::endl;
+}
+
+void Zombie::announce(){
+    std::cout << this->getName() << ": BraiiiiiiinnnzzzZ..." << std::endl;
+}
